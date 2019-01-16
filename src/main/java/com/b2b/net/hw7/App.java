9d49dd6c2436b714f0ee1e88db1e7b0b2a24c95a
apiszczek <antoni.piszczek@b2bnetwork.pl>
2019-01-16 11:21:20 +0100

    initial commit

---
+++ src/main/java/com/b2b/net/hw7/App.java
@@ -0,0 +1,13 @@
+package com.b2b.net.hw7;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/b2b/net/hw6/AppTest.java
@@ -0,0 +1,20 @@
+package com.b2b.net.hw6;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
